b6381be10383bd9adabf607abbda0caee39be187
Tomo-ja <tomonao.hj@gmail.com>
2022-03-04 22:02:51 -0800

    create user model

--- db/migrate/20220305055351_create_modals.rb
+++ db/migrate/20220305055351_create_modals.rb
@@ -3,11 +3,12 @@ class CreateModals < ActiveRecord::Migration[7.0]
     create_table :modals do |t|
       t.string :User
       t.string :name
-      t.string :email
+      t.string :email, :unique => true
       t.string :password_digest
       t.string :remember_digest
 
       t.timestamps
     end
+    add_index :users, :email
   end
 end
