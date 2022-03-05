92cf7e123d791d9bae227d7630e952728df75a07
Tomo-ja <tomonao.hj@gmail.com>
2022-03-05 10:30:30 -0800

    create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
+  get '/signup', to: 'users#new'
+  get '/profile', to: 'users#show'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
